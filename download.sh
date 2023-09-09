4bff70e1b5c7ab7ed229ddc62dafd5711cb974e9
uoosef <ghobadi@yekaan.ir>
2023-09-10 02:41:18 +0330

    bump core version to 1.5.3

--- download.sh
+++ download.sh
@@ -9,10 +9,10 @@ dl_and_chmod() {
 }
 
 download_bepass() {
-  dl_and_chmod arm64-v8a "https://github.com/uoosef/bepass/releases/download/v1.5.0/Bepass-linux-arm64.41ac51.zip"
-  dl_and_chmod armeabi-v7a "https://github.com/uoosef/bepass/releases/download/v1.5.0/Bepass-linux-arm7.41ac51.zip"
-  dl_and_chmod x86 "https://github.com/uoosef/bepass/releases/download/v1.5.0/Bepass-linux-386.41ac51.zip"
-  dl_and_chmod x86_64 "https://github.com/uoosef/bepass/releases/download/v1.5.0/Bepass-linux-amd64.41ac51.zip"
+  dl_and_chmod arm64-v8a "https://github.com/uoosef/bepass/releases/download/v1.5.3/Bepass-linux-arm64.4f27e7.zip"
+  dl_and_chmod armeabi-v7a "https://github.com/uoosef/bepass/releases/download/v1.5.3/Bepass-linux-arm7.4f27e7.zip"
+  dl_and_chmod x86 "https://github.com/uoosef/bepass/releases/download/v1.5.3/Bepass-linux-386.4f27e7.zip"
+  dl_and_chmod x86_64 "https://github.com/uoosef/bepass/releases/download/v1.5.3/Bepass-linux-amd64.4f27e7.zip"
 }
 
 download_"$1"
